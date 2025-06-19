aa58fb22266487ed98c760f2d8a11295f0f787c3
Eric Kong <erickongl@gmail.com>
2025-06-19 16:29:27 -0700

    Fixed random post button so that it would not go to the current post

--- src/utils/getRandomInt.ts
+++ src/utils/getRandomInt.ts
@@ -6,3 +6,20 @@
 export function getRandomInt(min: number, max: number) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+/*
+    getRandomIntExcludingCurrent
+    returns a random integer in [min, max)
+    inclusive of min but exclusive of max
+    and is guaranteed not to return current
+*/
+export function getRandomIntExcludingCurrent(min: number, max: number, current: number) {
+    if (max <= min + 1) {
+        return min;
+    } else if (current >= min && current < max) {
+        const intermediate = Math.floor(Math.random() * (max - min - 1)) + min;
+        return (intermediate >= current) ? intermediate + 1 : intermediate;
+    } else {
+        return getRandomInt(min, max);
+    }
+}
