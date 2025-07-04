40338ecd8fa8512f42f90ac92bcc251f70ff651c
Eric Kong <erickongl@gmail.com>
2025-07-04 01:45:08 -0700

    Added sitemap.xml and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  site: 'https://kittencarnival.com',
+  integrations: [sitemap()],
 });
